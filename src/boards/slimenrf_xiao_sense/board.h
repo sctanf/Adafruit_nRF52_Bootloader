87d8cfc868cf302e5add2b7e05a06282dd6069e5
sctanf <36978460+sctanf@users.noreply.github.com>
2025-06-19 03:05:13 -0500

    update boards from slimevr

--- src/boards/slimenrf_xiao_sense/board.h
+++ src/boards/slimenrf_xiao_sense/board.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2020 Nick Winans
+ * Copyright (c) 2018 Ha Thach for Adafruit Industries
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,45 +22,48 @@
  * THE SOFTWARE.
  */
 
-#ifndef _SLIMENRF_TRK_PROMICRO_H
-#define _SLIMENRF_TRK_PROMICRO_H
+#ifndef _XIAO_BLE_SENSE
+#define _XIAO_BLE_SENSE
 
 #define _PINNUM(port, pin)    ((port)*32 + (pin))
-
-#define UICR_REGOUT0_VALUE UICR_REGOUT0_VOUT_2V1
 
 /*------------------------------------------------------------------*/
 /* LED
  *------------------------------------------------------------------*/
-#define LEDS_NUMBER       1
-#define LED_PRIMARY_PIN   _PINNUM(0, 15) // Blue "Red"
-#define LED_STATE_ON      1
+// The board has 3 leds, but changing the number here causes OTA issues.
+#define LEDS_NUMBER           1
+#define LED_PRIMARY_PIN       _PINNUM(0, 26)
+#define LED_STATE_ON          0
+
+#define NEOPIXELS_NUMBER      0
 
 /*------------------------------------------------------------------*/
 /* BUTTON
  *------------------------------------------------------------------*/
-#define BUTTONS_NUMBER    2  // none connected at all
-#define BUTTON_1          _PINNUM(0, 18)  // unusable: RESET
-#define BUTTON_2          _PINNUM(0, 19)  // no connection
-#define BUTTON_PULL       NRF_GPIO_PIN_PULLUP
+#define BUTTONS_NUMBER        2
+#define BUTTON_1              _PINNUM(0, 18)
+// Button 2 is defined as D1 from expansion board.
+#define BUTTON_2              _PINNUM(0, 3)
+#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
 
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
-#define BLEDIS_MANUFACTURER  "SlimeVR"
-#define BLEDIS_MODEL         "SlimeNRF Tracker ProMicro"
+#define BLEDIS_MANUFACTURER   "Seeed"
+#define BLEDIS_MODEL          "SlimeNRF Tracker XIAO Sense"
 
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
-// keep using original USB_DESC from nice!nano/clones
-#define USB_DESC_VID           0x239A
-#define USB_DESC_UF2_PID       0x00B3
-#define USB_DESC_CDC_ONLY_PID  0x00B3
+// keep using original USB_DESC
+#define USB_DESC_VID           0x2886
+#define USB_DESC_UF2_PID       0x0045
+#define USB_DESC_CDC_ONLY_PID  0x0045
 
-#define UF2_PRODUCT_NAME  "SlimeVR SlimeNRF Tracker ProMicro"
+//------------- UF2 -------------//
+#define UF2_PRODUCT_NAME  "Seeed Studio SlimeNRF Tracker XIAO Sense"
 #define UF2_VOLUME_LABEL  "SLIMENRFTRK"
-#define UF2_BOARD_ID      "nRF52840-SlimeNRFTracker-ProMicro"
+#define UF2_BOARD_ID      "nRF52840-SlimeNRFTracker-XiaoSense"
 #define UF2_INDEX_URL     "https://github.com/SlimeVR"
 
-#endif // _SLIMENRF_TRK_PROMICRO_H
+#endif // _XIAO_BLE_SENSE
